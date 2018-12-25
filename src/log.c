c8c2558209fe4937c574418776bf095633181a0c
Paul <paul.becker@tu-dortmund.de>
2018-12-25 03:36:25 +0100

    fix bug in log.c

--- src/log.c
+++ src/log.c
@@ -1,13 +1,13 @@
 #include "log.h"
 
 void log_info(const char *info) {
-    printf("[I]: %s\n", info)
+    printf("[I]: %s\n", info);
 }
 
 void log_error(const char *error) {
-    printf("[E]: %s\n", error)
+    printf("[E]: %s\n", error);
 }
 
 void log_debug(const char *debug) {
-    printf("[D]: %s\n", msg)
+    printf("[D]: %s\n", debug);
 }
