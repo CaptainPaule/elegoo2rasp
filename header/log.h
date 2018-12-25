ad1eb27ddcedb29ac6565fb626cb035ff41cf42a
Paul <paul.becker@tu-dortmund.de>
2018-12-25 03:34:19 +0100

    add basic command line handling and logging

---
+++ header/log.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+// basic logging
+void log_info(const char *info);
+void log_error(const char *error);
+void log_debug(const char *debug);
--- src/serial.c
+++ src/serial.c
@@ -1,6 +1,20 @@
-#include <stdio.h>
+#include <log.h>
+#include <sys/stat.h>
 
 int main(int argc, char *argv[]) {
-    printf("Hello World\n");
-    return;
+    if(argc != 1) {
+        log_error("wrong number of arguments!");
+        return -1;
+    }
+
+    log_info("looking for serial port %s", argv[0]);
+    struct stat buffer;
+    int exist = stat(argv[0], &buffer);
+    if(exist != 0) {
+        log_error("serial port not found!");
+        return -1;
+    }
+
+    log_info("open connection...");
+    return 0;
 }
