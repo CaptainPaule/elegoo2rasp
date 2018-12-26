04ff9e05d48ec6870e76c3a9f39bc68484e44ffb
Paul <paul.becker@tu-dortmund.de>
2018-12-26 12:48:34 +0100

    update serial.c

--- src/serial.c
+++ src/serial.c
@@ -1,5 +1,11 @@
 #include <log.h>
 #include <sys/stat.h>
+#include <stdio.h>   /* Standard input/output definitions */
+#include <string.h>  /* String function definitions */
+#include <unistd.h>  /* UNIX standard function definitions */
+#include <fcntl.h>   /* File control definitions */
+#include <errno.h>   /* Error number definitions */
+#include <termios.h> /* POSIX terminal control definitions */
 
 int main(int argc, char *argv[]) {
     if(argc != 2) {
@@ -9,12 +15,27 @@ int main(int argc, char *argv[]) {
 
     printf("[I]: looking for serial port %s\n", argv[1]);
     struct stat buffer;
-    int exist = stat(argv[0], &buffer);
+    int exist = stat(argv[1], &buffer);
     if(exist != 0) {
         log_error("serial port not found!");
         return -1;
     }
 
-    log_info("open connection...");
+    log_info("open port...");
+    int fd = open(argv[1], O_RDWR | O_NOCTTY | O_NDELAY);
+    if(fd == -1) {
+    	log_error("unable to open port");
+	return -1;
+    }
+
+    log_info("setup control structure");
+    struct termios options;
+    tcgetattr(fd, &options);
+    cfsetispeed(&options, B9600);
+    cfsetospeed(&options, B9600);
+    options.c_flag |= (CLOCAL | CREAD);
+    tcsetattr(fd, TCSANOW, &options);
+
+    
     return 0;
 }
